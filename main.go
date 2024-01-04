55d6487d8821fdbb1315633bd17ef6992b90513e
ValiantHarold <25witt25@gmail.com>
2024-01-03 21:37:11 -0700

    Go Web Socket Start

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	setupAPI()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setupAPI() {
+	manager := NewManager()
+
+	http.HandleFunc("/websocket", manager.serveWS)
+}
